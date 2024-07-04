cf18fea6bd120959cacb36c9ebbaef85d2fb62fc
anthony8choa <128433614+anthony8choa@users.noreply.github.com>
2024-07-04 11:13:39 -0600

    segundo commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,10 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    //protected $primaryKey = 'idDuenios';
+    //protected $primaryKey = 'numeroCuenta';
+    //public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
