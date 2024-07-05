9b25f888d1a33c2de6c6bd9a370c398102309704
anthony8choa <128433614+anthony8choa@users.noreply.github.com>
2024-07-04 19:03:12 -0600

    prueba de examen

---
+++ app/Http/Controllers/directoriosController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\directorio;
+
+use Illuminate\Http\Request;
+
+class directoriosController extends Controller
+{
+    public function creardirectorio(){
+        return view('CrearEntrada');
+    }
+
+    public function guardar(Request $request){
+        $nvoDirectorio = new directorio();
+        $nvoDirectorio->codigoEntrada = $request->codigo;
+        $nvoDirectorio->nombre = $request->nombre;
+        $nvoDirectorio->apellido = $request->apellido;
+        $nvoDirectorio->telefono = $request->telefono;
+        $nvoDirectorio->correo = $request->correo;
+        $nvoDirectorio->save();
+        return redirect('/directorio/mostrar');
+    }
+     
+    public function mostrardirectorio(){
+        $directorios = directorio::all();
+      
+        return view('directorio', compact('directorios'));
+
+    }
+
+    public function eliminar($id){
+        $directorioEliminar = directorio::find($id);
+        return view('eliminar', compact('directorioEliminar'));
+    }
+
+    public function destroy($id){
+        $directorioEliminar = directorio::find($id);
+        $directorioEliminar->delete();
+        return redirect('/directorio/mostrar');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\directoriosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/directorio/crear',[directoriosController::class,'creardirectorio'])->name('directorio.crear');
+
+Route::post('/directorio/guardar',[directoriosController::class,'guardar'])->name('directorios.guardar');
+
+Route::get('/directorio/mostrar',[directoriosController:: class,'mostrardirectorio']);
+
+
+Route::get('/directorio/eliminar/{id}',[directoriosController:: class,'eliminar'])->name('directorios.eliminar');
+
+Route::get('/duenios/destroy/{id}',[directoriosController:: class,'destroy'])->name('directorios.destruir');
